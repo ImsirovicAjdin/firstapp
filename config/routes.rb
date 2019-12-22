ecdcb35b9b188c25361ae50b81505dec11acb6e3
ImsirovicAjdin <ajdika@live.com>
2019-12-22 18:35:35 +0100

    Working with routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'play/index'
+  # get 'play/index'
+  # get ':controller(/:action(/:id))'
+  match "/", :to => "play#index", :via => :get
+  match ':controller(/:action(/:id))', :via => :get
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
