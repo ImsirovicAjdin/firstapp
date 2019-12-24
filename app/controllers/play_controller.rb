4f2f6e21d29c15dd1924c9e5de19b89b0c7f03f1
ImsirovicAjdin <ajdika@live.com>
2019-12-24 07:21:35 +0100

    Routes and redirects

--- app/controllers/play_controller.rb
+++ app/controllers/play_controller.rb
@@ -1,4 +1,16 @@
 class PlayController < ApplicationController
   def index
   end
+  def home
+    redirect_to(
+      :controller => 'another', 
+      :action => 'index')
+  end
+  def house
+    redirect_to(
+      :action => 'index')
+  end
+  def google
+    redirect_to('https://google.com')
+  end
 end
