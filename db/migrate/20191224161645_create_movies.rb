d2858a516e90ee305b92b6117296b00991fad837
ImsirovicAjdin <ajdika@live.com>
2019-12-24 21:59:08 +0100

    Generate movies model and add two migrations

---
+++ db/migrate/20191224161645_create_movies.rb
@@ -0,0 +1,15 @@
+class CreateMovies < ActiveRecord::Migration[6.0]
+  def up
+    create_table :movies do |t|
+      t.column "title", :string, :limit => 150
+      t.column "description", :string, :limit => 300
+      t.column "rating", :integer, :limit => 5
+      t.timestamps
+    end
+  end
+
+  def down
+    drop_table :movies
+  end
+
+end
