0782e910057849fde50b0ec6d819465d31c21215
ImsirovicAjdin <ajdika@live.com>
2019-12-26 12:12:31 +0100

    Named scopes

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,7 @@
 class Movie < ApplicationRecord
+    # self.primary_key = 'id'
+
+    scope :from_newest, lambda { order("created_at DESC") }
+    scope :lookup, lambda {|titlestring| where(["title LIKE ?", "%#{titlestring}%"]) }
+    
 end
